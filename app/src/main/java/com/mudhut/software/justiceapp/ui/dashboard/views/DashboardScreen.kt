9ed2acfab82240052729cbc8701537259ac9db62
Phillip Seryazi <seryaziphillip@gmail.com>
2022-01-17 18:50:15 +0300

    organising bottom bar

--- app/src/main/java/com/mudhut/software/justiceapp/ui/dashboard/views/DashboardScreen.kt
+++ app/src/main/java/com/mudhut/software/justiceapp/ui/dashboard/views/DashboardScreen.kt
@@ -1,8 +1,8 @@
 package com.mudhut.software.justiceapp.ui.dashboard.views
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -19,6 +20,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.mudhut.software.justiceapp.R
 import com.mudhut.software.justiceapp.navigation.Destination
 import com.mudhut.software.justiceapp.navigation.bottomBarGraph
 import com.mudhut.software.justiceapp.ui.dashboard.models.BottomNavItem
@@ -33,29 +35,56 @@ fun DashboardScreen() {
         modifier = Modifier.fillMaxSize(),
         scaffoldState = scaffoldState,
         bottomBar = {
-            BottomNavigationBar(
-                navItems = listOf(
-                    BottomNavItem(
-                        name = "Home",
-                        route = Destination.HomeScreen.route,
-                        icon = Icons.Filled.Home
+            BottomAppBar(
+                backgroundColor = MaterialTheme.colors.primarySurface,
+                cutoutShape = CircleShape,
+                elevation = 2.dp
+            ) {
+                BottomNavigationBar(
+                    navItems = listOf(
+                        BottomNavItem(
+                            name = "Home",
+                            route = Destination.HomeScreen.route,
+                            icon = Icons.Filled.Home
+                        ),
+                        BottomNavItem(
+                            name = "Inbox",
+                            route = Destination.NotificationScreen.route,
+                            icon = Icons.Filled.Email
+                        ),
+                        BottomNavItem(
+                            name = "Profile",
+                            route = Destination.ProfileScreen.route,
+                            icon = Icons.Filled.Person
+                        ),
+                        BottomNavItem(
+                            name = "",
+                            route = "",
+                            icon = null
+                        )
                     ),
-                    BottomNavItem(
-                        name = "Notifications",
-                        route = Destination.NotificationScreen.route,
-                        icon = Icons.Filled.Email
-                    ),
-                    BottomNavItem(
-                        name = "Profile",
-                        route = Destination.ProfileScreen.route,
-                        icon = Icons.Filled.Person
-                    )
-                ),
-                navController = navController,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 0.dp, end = 0.dp)
-            )
+                    navController = navController,
+                    modifier = Modifier
+                        .padding(
+                            start = 0.dp,
+                            end = 0.dp
+                        )
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        isFloatingActionButtonDocked = true,
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = CircleShape,
+                contentColor = Color.White,
+                onClick = { })
+            {
+                Icon(
+                    painter = painterResource(R.drawable.ic_camera),
+                    contentDescription = null
+                )
+            }
         }
     ) {
         NavHost(
@@ -76,17 +105,16 @@ fun BottomNavigationBar(
 ) {
     BottomNavigation(
         modifier = modifier,
-        backgroundColor = Color.Transparent,
-        elevation = 2.dp
     ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentDestination = navBackStackEntry?.destination?.route
         navItems.forEach { screen ->
             BottomNavigationItem(
+                label = { Text(screen.name) },
                 selected = currentDestination == screen.route,
-                selectedContentColor = MaterialTheme.colors.primary,
-                unselectedContentColor = Color.Black,
-                icon = { Icon(screen.icon, contentDescription = null) },
+                selectedContentColor = MaterialTheme.colors.secondaryVariant,
+                unselectedContentColor = Color.White,
+                icon = { screen.icon?.let { Icon(it, contentDescription = null) } },
                 onClick = {
                     navController.navigate(screen.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
