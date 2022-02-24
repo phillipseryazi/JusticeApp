093dc0cfe91708bebb8fdd3950ec030d84e2df5e
Phillip Seryazi <seryaziphillip@gmail.com>
2022-02-24 13:05:53 +0300

    refactor comment screen 1

--- app/src/main/java/com/mudhut/software/justiceapp/ui/dashboard/views/CommentScreen.kt
+++ app/src/main/java/com/mudhut/software/justiceapp/ui/dashboard/views/CommentScreen.kt
@@ -1,20 +1,26 @@
 package com.mudhut.software.justiceapp.ui.dashboard.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mudhut.software.justiceapp.R
 import com.mudhut.software.justiceapp.ui.theme.JusticeAppTheme
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun CommentScreen(
@@ -63,7 +69,8 @@ fun TopBarSectionComposable(navigateBack: () -> Unit) {
             style = MaterialTheme.typography.h1,
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp,
-            color = Color.Blue
+            color = Color.Blue,
+            textAlign = TextAlign.Center,
         )
     }
 }
@@ -72,6 +79,34 @@ fun TopBarSectionComposable(navigateBack: () -> Unit) {
 fun CommentListSectionComposable() {
 
 }
+
+@Composable
+fun CommentComposable(avatar: Int, author: String, comment: String) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row() {
+            GlideImage(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape),
+                imageModel = avatar,
+                contentScale = ContentScale.Fit,
+            )
+            Column {
+                Text(
+                    author,
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    comment,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
 
 @Preview
 @Composable
